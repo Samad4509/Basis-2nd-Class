fca8b955616e38dbdfa42df82925510b6cc5bf72
unknown <samad4509@gmail.com>
2023-04-10 17:40:27 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>2nd-class</title>
+    </head>
+    <body>
+        <h1>This my table tag..</h1>
+
+    {{--    Table-1--}}
+    <table border="1">
+        <tr>
+            <td width="300" height="200">
+
+            </td>
+        </tr>
+    </table>
+    <br>
+    {{--    Table-2--}}
+    <table border="1" width="300">
+        <tr>
+            <td height="200"></td>
+            <td></td>
+        </tr>
+    </table>
+        <br>
+    {{--    Table-3--}}
+    <table border="1" width="300" height="200">
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+        <br>
+        {{--    Table-4--}}
+    <table border="1" width="300" height="200">
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>
+    </table>
+    </body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Layout</title>
+    </head>
+    <body>
+        <table border="1" width="1000" align="center">
+            <tr>
+                <td width="200" height="200"></td>
+                <td ></td>
+            </tr>
+            <tr>
+                <td colspan="2" height="50"></td>
+            </tr>
+            <tr>
+                <td height="600"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="2" height="50"></td>
+            </tr>
+        </table>
+    </body>
+</html>
